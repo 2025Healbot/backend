4e6cc5869a7e1ff57006de01497b4145bd2479b8
DESKTOP-FEP78G5\user <kjh40607@gmail.com>
2025-11-17 17:36:40 +0900

    [Modify] 관리자 관련 코드 수정
    MemberController : adminYn으로 기본값 지정
    MemberService : findByMemberId 메소드 추가
    MemberServiceImpl : findByIdOnly 메소드 추가

--- src/main/java/com/hospital/boot/domain/member/model/service/MemberService.java
+++ src/main/java/com/hospital/boot/domain/member/model/service/MemberService.java
@@ -5,6 +5,7 @@ import com.hospital.boot.domain.member.model.vo.Member;
 public interface MemberService {
     String getSocialUserId(String type, String code);
     Member findBySocialId(String socialId);
+    Member findByMemberId(String memberId);
     int signup(Member member);
     boolean checkId(String id);
     String findId(String name, String email);
