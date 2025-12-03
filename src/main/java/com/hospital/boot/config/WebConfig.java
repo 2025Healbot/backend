f41157c96904a63fbc6a3d4c81f83cdb1827dfd8
asdniofo <ceyeun33@jmail.ac.kr>
2025-12-03 12:50:40 +0900

    [MODIFY] 서버 설정도 포함

--- src/main/java/com/hospital/boot/config/WebConfig.java
+++ src/main/java/com/hospital/boot/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:80","http://localhost")
+                .allowedOrigins("https://localhost","https://healbot.online","http://localhost:80","http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
